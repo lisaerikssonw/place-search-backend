b940f11eaf3e11a68d472789bd302b51c3e59daa
Kenneth Österholm <kenneth.osterholm@weahead.se>
2019-06-13 10:22:57 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 5000
+const apiRoutes = require('./routes/api')
+
+// Starts the server on the defined port
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+/* Middlewares */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/* Routes */
+app.use('/api', apiRoutes)
+
+app.use(express.static(path.join(__dirname, 'client/build')))
